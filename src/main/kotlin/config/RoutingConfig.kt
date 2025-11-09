4ee5e6e0b0433def5ece363af655d4d76fdd4604
Andreas M Lbn Gaol <lgandre45@gmail.com>
2025-11-10 00:17:44 +0700

    Redirect root path to API endpoint and update deployment script

--- src/main/kotlin/config/RoutingConfig.kt
+++ src/main/kotlin/config/RoutingConfig.kt
@@ -2,6 +2,7 @@ package com.sukakotlin.config
 
 import com.sukakotlin.features.user.presentation.routes.userRoutes
 import io.ktor.server.application.Application
+import io.ktor.server.response.respondRedirect
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
@@ -9,6 +10,7 @@ import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
+        get("/") { call.respondRedirect("/api") }
         route("/api") {
             get {
                 call.respondText("Hello World!")
